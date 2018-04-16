ac2eb030db940a14d60d0ed687d013e2dc4aa324
Alan Yeats <alan@alanyeats.com>
2018-04-16 18:04:54 +0100

    Updated README
    Added License

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # Easing Functions For C# and Unity
+Implementation of Easing Functions within Unity in C#
 
-All 40 Easing functions simply implemented into C# for Unity.
+Sine, Quad, Cubic, Quint, Expo, Circ, Back, Elastic & Bounce each with there in and out variants. 
 
 
+
+# Usage 
+
+All easing functions can be called from the following static function 
+
+    EasingLerps.EasingLerp(EasingLerpsType type, EasingInOutType inOutType, float time, float a, float b)
+
+Where 
+    type being type of easing to use (Sine, Quad ect)
+    inOutType being which variant for use
+    time being the time between 0 and 1
+    a being the start lerp value 
+    b being the ending lerp value
+
+with it returning a float value between a and b for the given time value.
+
+# Demo
+
+All functionality can be played around with from the EasingLerpExample example scene. 
+
+Loading and playing this scene will allow you to jump between each type of easing function along with it's in & out variation.
+
+
+# License
+
+Developed by Alan Yeats with a zlib license. While the zlib license does not require acknowledgement.
--- Scripts/EasingLerps.cs
+++ Scripts/EasingLerps.cs
@@ -1,7 +1,26 @@
-﻿using System;
+﻿/*
+* Copyright (c) 2017-2018 Alan Yeats http://alanyeats.com
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 /// <summary>
 /// Easing Lerp Implmentation to create effective lerp functions 
 /// 
@@ -39,7 +58,7 @@ class EasingLerps
     /// Easy use of all Easing lerp function   
     /// </summary>
     /// <param name="type">The type of Lerp</param>
-    /// <param name="inOutType">Which type of effet</param>
+    /// <param name="inOutType">Which type of effect</param>
     /// <param name="time">Current time</param>
     /// <param name="a">Starting Value</param>
     /// <param name="b">Ending Value</param>
